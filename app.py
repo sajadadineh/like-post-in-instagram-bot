696af7e6c00d0dff8d83b438ed62dfdc9f64d0bc
sajadadineh <sajadadineh@protonmail.com>
2020-03-27 19:00:52 +0430

    Optimizing and beautiful code

--- app.py
+++ app.py
@@ -3,24 +3,19 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 import constants
-import pass
+
+username = raw_input("please Enter your username : ")
+password = raw_input("please Enter your password : ")
 
 option = webdriver.ChromeOptions()
 # option.add_argument('headless')
-driver = webdriver.Chrome(Chrome_options=option, executable_path='./chromedriver')
+driver = webdriver.Chrome(chrome_options=option, executable_path='./chromedriver')
 driver.get(constants.base_url)
 
 def login():
     driver.implicitly_wait(20)
-    driver.find_element_by_xpath(constants.log_in).click()
-    sleep(2)
-    username_element = driver.find_element_by_xpath(constants.username)
-    username_element.clear()
-    username_element.send_keys(pass.username)
-    password_element = driver.find_element_by_xpath(constants.password)
-    password_element.clear()
-    password_element.send_keys(pass.password)
-    password_element.send_keys(Keys.ENTER)
+    driver.find_element_by_xpath(constants.username).send_keys(username)
+    driver.find_element_by_xpath(constants.password).send_keys(password,Keys.ENTER)
     sleep(2)
 
 def notNowButton():
@@ -45,6 +40,6 @@ def closeDriver():
     driver.close()
 
 login()
-notNowButton()
-findHashtag()
-clickPostAndLike()
+# notNowButton()
+# findHashtag()
+# clickPostAndLike()
--- constants.py
+++ constants.py
@@ -2,7 +2,6 @@ base_url = "https://www.instagram.com/"
 hashtag_url = "https://www.instagram.com/explore/tags/{}/"
 
 # xpah
-log_in = "//a[@href='/accounts/login/?source=auth_switcher']"
 username = "//input[@name='username']"
 password = "//input[@name='password']"
 not_now_button = "//button[@class='aOOlW   HoLwm ']"
